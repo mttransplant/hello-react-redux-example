818cf5e91eeb9faa3d05719aef53e3f77c3ff60e
Adam Smith <apsmith@ccs.neu.edu>
2018-10-07 14:24:09 -0400

    implemented basic starting point

---
+++ src/containers/HelloContainer.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+import Hello from "../components/HelloComponent";
+
+class HelloWorld extends Component {
+    render() {
+        return <Hello/>
+    }
+}
+export default HelloWorld
